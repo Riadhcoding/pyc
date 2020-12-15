574e6f136cd32dcfb473476e365c7666d977882f
python life <71737254+python-life@users.noreply.github.com>
2020-12-15 00:20:27 +0000

    Update pyc.py

--- pyc.py
+++ pyc.py
@@ -28,12 +28,12 @@ print('\033[1;31m[2] \033[1;35mDecode  | pyc to py'),time.sleep(0.3)
 print('\033[1;36m+++++++++++++++++++++++++++++++++++++++'+a),time.sleep(0.3)
 choose =input('\n\033[1;31m[?]Choose : \033[1;37m')
 if choose =='1':
-    file_py = input(" \033[1;32mFile >\033[1;37m")
+    file_py = input("\n\033[1;32mFile >\033[1;37m")
     compileall.compile_file(file_py)
     print('\033[1;31msuccess encrypt')
     print("\033[1;36mSave to /sdcard/__pycache__")
 elif choose =='2':
-    file_pyc = input('\033[1;32mFile >\033[1;37m')
+    file_pyc = input('\n\033[1;32mFile >\033[1;37m')
     com = f'uncompyle6 {file_pyc} > {file_pyc}_dec.py'
     os.system(com)
     print('\033[1;31mdecoding success')
